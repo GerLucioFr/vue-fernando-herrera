ca6b3446f304c1d640a7f56fd88b61c2464920cb
Ger Lucio <gerardo.lucio@umvel.com>
2023-05-05 12:55:59 -0600

    feat: install axios and indecision app created

--- 03-fundamentos-vue/src/App.vue
+++ 03-fundamentos-vue/src/App.vue
@@ -1,34 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-
   <div class="container">
-    <CounterComponent title="Get in" :start="1" />
-    <CounterComponent title="Get out" />
+    <!-- <CounterComponent title="Get in" :start="1" />
+    <CounterComponent title="Get out" /> -->
+    <IndecisionComponent />
   </div>
 </template>
 
 <script>
-import CounterComponent from './components/CounterComponent.vue';
+import IndecisionComponent from './components/IndecisionComponent.vue';
 export default {
   name: 'App',
   components: {
-    CounterComponent,
+    IndecisionComponent,
   },
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-.container {
-  display: flex;
-  justify-content: space-around;
-}
-</style>
+<style></style>
---
+++ 03-fundamentos-vue/src/components/IndecisionComponent.vue
@@ -0,0 +1,120 @@
+<template>
+  <div class="indecision-container">
+    <img
+      :src="
+        questionAnswer.image ||
+        'https://w.wallhaven.cc/full/96/wallhaven-966re1.jpg'
+      "
+      alt="background"
+    />
+    <div class="indecision-form">
+      <input
+        type="text"
+        v-model="question"
+        :disabled="isLoading"
+        placeholder="Ask me anything?"
+      />
+      <p>Remember to use a question mark at the end</p>
+
+      <div class="question">
+        <h2>{{ question || 'Your question goes here' }}</h2>
+
+        <h1 v-if="isLoading">Loading your answer :)</h1>
+        <h1 v-if="!isLoading && questionAnswer.answer">
+          {{ questionAnswer.answer }}
+        </h1>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import axios from 'axios';
+export default {
+  name: 'IndecisionComponent',
+  data() {
+    return {
+      question: '',
+      isLoading: false,
+      questionAnswer: {
+        answer: '',
+        forced: false,
+        image: '',
+      },
+    };
+  },
+  watch: {
+    question(value) {
+      if (!value.includes('?')) {
+        this.resetAnswer();
+        return;
+      }
+      this.getAnswer();
+    },
+  },
+  methods: {
+    async getAnswer() {
+      const baseURL = 'https://yesno.wtf/api';
+      const axiosInstance = axios.create({
+        baseURL,
+      });
+
+      this.isLoading = true;
+      try {
+        const { data } = await axiosInstance.get();
+
+        this.questionAnswer = data;
+        this.isLoading = false;
+      } catch (error) {
+        this.isLoading = false;
+        console.error(error);
+      }
+    },
+    resetAnswer() {
+      this.questionAnswer = {
+        answer: '',
+        forced: false,
+        image: '',
+      };
+    },
+  },
+};
+</script>
+<style scoped>
+.indecision-container {
+  box-sizing: border-box;
+  position: relative;
+  width: 100%;
+  height: 100vh;
+}
+
+img {
+  position: absolute;
+  top: 0;
+  left: 0;
+
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+
+  opacity: 0.5;
+
+  z-index: 1;
+}
+
+.indecision-form {
+  position: relative;
+
+  margin: 32px 0;
+
+  z-index: 2;
+}
+
+input[type='text'] {
+  padding: 8px;
+  border-radius: 4px;
+}
+
+.question {
+  margin-top: 64px;
+}
+</style>
